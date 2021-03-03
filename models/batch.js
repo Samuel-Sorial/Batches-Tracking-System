a116da93fe9fd2ec495bf91b5ba2b5a0485b3c63
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-03 19:24:25 +0200

    Adding virtual name for number, and a function to auto convert object id to number

--- models/batch.js
+++ models/batch.js
@@ -1,4 +1,18 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-param-reassign */
 const mongoose = require('mongoose');
+
+/**
+ * Converts object id to a number
+ * @param {mongoose.Types.ObjectId} id
+ * @returns {Number} unique number that always will refer to same object
+ */
+const convertIdToNumber = (id) => {
+  // Cut the id and only use PID, INC of this object id,
+  // for more information: https://stackoverflow.com/a/58024069/13089670
+  const number = id.toHexString().substr(14);
+  return number;
+};
 
 const batchSchema = new mongoose.Schema({
   size: { type: String, enum: ['S', 'M', 'L', 'XL'], required: true },
@@ -8,6 +22,17 @@ const batchSchema = new mongoose.Schema({
     required: true,
   },
   quantity: { type: Number, min: 1, index: 1 },
+});
+
+batchSchema.virtual('number').get(function () {
+  return convertIdToNumber(this._id);
+});
+
+batchSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.number = convertIdToNumber(returnedObject._id);
+    delete returnedObject.__v;
+  },
 });
 
 module.exports = mongoose.model('Batch', batchSchema);
