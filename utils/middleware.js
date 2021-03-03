8d4da9973c2b28befde2416e65e455453f8c2239
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-03 18:21:14 +0200

    Adding middlewares to handle common cases, to log request data, and not found endpoints

---
+++ utils/middleware.js
@@ -0,0 +1,55 @@
+const logger = require('./logger');
+
+/**
+ * Loggs each coming request
+ * @module requestLogger
+ * @function
+ * @param {import('express').Request} request
+ * @param {import('express').Response} response
+ * @param {import('express').NextFunction} next
+ */
+const requestLogger = (request, response, next) => {
+  logger.info('Method: ', request.method);
+  logger.info('Path: ', request.path);
+  if (request.query) {
+    logger.info('Query: ', request.query);
+  }
+  logger.info('Body: ', request.body);
+  next();
+};
+
+/**
+ * Handles not found endpoint
+ * @module notFound
+ * @function
+ * @param {import('express').Request} request
+ * @param {import('express').Response} response
+ * @param {import('express').NextFunction} next
+ */
+const notFound = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+/**
+ * Handles errors
+ * @module errorHandler
+ * @function
+ * @param {Error} error
+ * @param {import('express').Request} request
+ * @param {import('express').Response} response
+ * @param {import('express').NextFunction} next
+ */
+const errorHandler = (error, request, response, next) => {
+  logger.error(error.message);
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' });
+  }
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
+  }
+
+  next(error);
+};
+
+module.exports = { requestLogger, notFound, errorHandler };
