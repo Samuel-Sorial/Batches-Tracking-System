a43229cb925d57a8d760cb34b748f002bf238ca4
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-03 20:41:02 +0200

    Adding more details to changelog, adding skeleton for batch controller

---
+++ controllers/batchController.js
@@ -0,0 +1,9 @@
+const Batch = require('../models/batch');
+
+/**
+ * Creates a new batch if the given data is valid
+ * @param {import('express').Request} request
+ * @param {import('express').Response} response
+ * @param {import('express').NextFunction} next
+ */
+module.exports.createBatch = (request, response, next) => {};
