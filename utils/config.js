4e90abb4263dee0044e73e5992e5a952cae6ba7c
Samuel-Sorial <eng.samuel.maged@gmail.com>
2021-03-03 18:40:35 +0200

    Adding development db configurations

--- utils/config.js
+++ utils/config.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 
 const { PORT } = process.env;
 
+// Determine which database to use
 let { MONGODB_URI } = process.env;
 if (process.env.NODE_ENV === 'test') {
-  MONGODB_URI = process.env.TEST_MONGODB;
+  MONGODB_URI = process.env.TEST_MONGODB_URI;
+} else if (process.env.NODE_ENV === 'development') {
+  MONGODB_URI = process.env.DEVELOPMENT_MONGODB_URI;
 }
 
 module.exports = { PORT, MONGODB_URI };
